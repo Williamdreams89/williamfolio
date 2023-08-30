6b7e9a277e49ce26cda6e103589ffe02961cebb4
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-08-30 12:31:33 +0000

    Description of me

---
+++ src/pages/about/Description.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Description = () => {
+  return (
+    <div>
+        <div>
+        <h2>My name is Danquah Kwafo William. I hail from Kumasi, Ghana.</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Description
